a25a68fd30bc952be4c98afd0c2ffcd3c41fe767
Яна <Yana_sin82@mail.ru>
2024-06-29 21:53:46 +0500

    Добавлено в Githab Module_2_2

---
+++ Module_2_2.py
@@ -0,0 +1,18 @@
+first=126
+second=543
+third=59
+if first==second==third:
+    print(3)
+if second==third:
+    print(2)
+if first!=second!=third:
+    print(0)
+first=12
+second=43
+third=43
+if first==second or second==third or first==third:
+    print(2)
+if first==second==third:
+    print(3)
+if first!=second!=third:
+    print(0)
